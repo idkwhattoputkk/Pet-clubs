3949cf6a13d19a29bd5dff1ef03605d21518f62f
emmanuel umania <dacha1252@gmail.com>
2019-08-18 11:33:46 -0500

    added the different attributes of the classes and modified the README

--- src/model/Club.java
+++ src/model/Club.java
@@ -1,5 +1,32 @@
 package model;
 
+/**
+ * Class that represents a club.
+ * @author emanuelumana
+ *
+ */
 public class Club {
+	
+	/**
+	 * the attributes of the class
+	 */
+	private String name, date, id, typePet;
 
+	/**
+	 * Constructor of the Club class.
+	 * @param name
+	 * @param date
+	 * @param id
+	 * @param typePet
+	 */
+	public Club(String name, String date, String id, String typePet) {
+		super();
+		this.name = name;
+		this.date = date;
+		this.id = id;
+		this.typePet = typePet;
+	}
+	
+	
+	
 }
--- src/model/Owner.java
+++ src/model/Owner.java
@@ -1,5 +1,14 @@
 package model;
 
+/**
+ * Class that represents the owner of a pet.
+ * @author emanuelumana
+ *
+ */
 public class Owner {
-
+	
+	/**
+	 * the attributes of the class
+	 */
+	private String id, fullName, birthdate, typePet;
 }
